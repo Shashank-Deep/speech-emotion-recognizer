401e7edf507d25b5a1caa8e7fade83b726acb2e8
Shashank Deep <shashankdeep.647@gmail.com>
2023-06-22 17:06:58 +0530

    adding flask file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import numpy as np
 import librosa
-import streamlit as st
 import tensorflow as tf
 import io
-import sounddevice as sd
-import soundfile as sf
+# import sounddevice as sd
+# import soundfile as sf
+from flask import Flask, render_template, request
 
 # Load the model
 # replace 'my_model.h5' with your own saved model
@@ -26,52 +26,27 @@ def extract_mfcc(audio_bytes, sr):
     mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
     return mfcc
 
-# Define Streamlit app
-st.set_page_config(page_title='Emotion Recognition', page_icon=':microphone:', layout='wide')
-st.title('Emotion Recognition')
-st.markdown('## Upload a .wav file or record your voice to recognize the emotion')
-col1, col2 = st.columns(2)
+app = Flask(__name__)
 
-# Get file from user
-uploaded_file = col1.file_uploader('Upload .wav file', type='wav')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'audio' in request.files:
+            audio_file = request.files['audio']
+            audio_bytes = audio_file.read()
 
-# If file is uploaded
-if uploaded_file is not None:
-    # Read the audio file as bytes
-    audio_bytes = uploaded_file.read()
+            # Extract MFCC features
+            sr = None
+            mfcc = extract_mfcc(audio_bytes, sr)
+            mfcc = np.reshape(mfcc, newshape=(1, 40))
 
-    # Extract MFCC features
-    sr = None
-    mfcc = extract_mfcc(audio_bytes, sr)
-    mfcc = np.reshape(mfcc, newshape=(1, 40))
+            # Make prediction using the model
+            predictions = model.predict(mfcc)
+            emotion = emotions[np.argmax(predictions[0])]
 
-    # Make prediction using the model
-    predictions = model.predict(mfcc)
-    emotion = emotions[np.argmax(predictions[0])]
+            return render_template('index.html', emotion=emotion)
 
-    # Display the predicted emotion
-    col2.success('Predicted emotion: {}'.format(emotion))
+    return render_template('index.html')
 
-# Record audio
-recording = col1.button('Record')
-if recording:
-    duration = 3  # seconds
-    sr = 44100
-    myrecording = sd.rec(int(duration * sr), samplerate=sr, channels=1)
-    with st.spinner('Recording...'):
-        sd.wait()
-    sf.write('temp.wav', myrecording, sr)
-    audio_bytes = io.BytesIO()
-    with open('temp.wav', 'rb') as f:
-        audio_bytes.write(f.read())
-
-    # Extract MFCC features
-    mfcc = extract_mfcc(audio_bytes.getvalue(), sr)
-    mfcc = np.reshape(mfcc, newshape=(1, 40))
-
-    # Make prediction using the model
-    predictions = model.predict(mfcc)
-    emotion = emotions[np.argmax(predictions[0])]
-
-    # Display the predicted emotion
-    col2.success('Predicted emotion: {}'.format(emotion))
+if __name__ == '__main__':
+    app.run(debug=True)
